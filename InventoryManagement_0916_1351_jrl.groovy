7a1c2091ed27e6cc41db2f135bc855e9d11e9596
ezrAGraySon256628 <panoply_sullen.0y@icloud.com>
2025-09-16 14:51:55 +0900

    Enhance 库存管理系统 using GRAILS

---
+++ InventoryManagement_0916_1351_jrl.groovy
@@ -0,0 +1,7 @@
+// 代码生成时间: 2025-09-16 13:51:54
+// InventoryManagement.groovy\
+\
+// 库存管理系统的Grails服务类\
+class InventoryService {\
+    // 成功的消息模板\
+    public static final String SUCCESS_MESSAGE = "操作成功。\
